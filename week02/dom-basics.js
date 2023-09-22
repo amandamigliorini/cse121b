a28db70a611a8bb0022b0c1e6ba8a2c434227652
amandamigliorini <amanda.migliorini@hotmail.com>
2023-09-22 08:36:14 -0300

    W02-task.js's assignment is added and updated

--- week02/dom-basics.js
+++ week02/dom-basics.js
@@ -8,11 +8,13 @@ document.body.appendChild(newImg)
 const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
-const newSection = document.createElement("section")
-const newH2 = document.createElement("h2")
-const newPara = document.createElement("p")
-newH2.innerText = "CSE 121b"
-newPara.innerText = "Welcome to Javascript Language"
-newSection.appendChild(newH2)
-newSection.appendChild(newPara)
-document.body.appendChild(newSection)
+const homeWard = 3
+const ward = `<strong>Home Ward: ${homeWard}</strong>`;
+const newSection = document.createElement("section");
+const newH2 = document.createElement("h2");
+const newPara = document.createElement("p");
+newH2.innerHTML = "CSE 121b";
+newPara.innerText = ward;
+newSection.appendChild(newH2);
+newSection.appendChild(newPara);
+document.body.appendChild(newSection);
