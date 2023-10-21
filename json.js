4ef21e09ade1ad8b02e9b06fcfd09099236c344c
amandamigliorini <amanda.migliorini@hotmail.com>
2023-10-21 18:54:48 -0300

    Adding images and json changes

--- json.js
+++ json.js
@@ -1,7 +1,7 @@
 [
     {
         "recipeName":"Brownie",
-        "category": ["Dessert"],
+        "category": ["Desserts"],
         "ingredients": ["150g Dark Chocolate", "50g Butter", " 120g brown sugar", "60g Sugar"],
         "description": 
                         {1: "In a pan add the Chocolate and butter and heat in a water bath until you obtain a homogeneous cream.",
@@ -11,6 +11,19 @@
                         5: "Pour the dough into a rectangular shape pan, greased with butter and sprinkled with wheat flour and place in a preheated medium oven (180°C) for around 20 minutes.",
                         6: "Unmold and let it cool. Cut in squares and serve."
                     },
-        "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
-    }
+        "imageUrl": "https://github.com/amandamigliorini/cse121b/blob/main/images/pexels-alesia-kozik-6072096.jpg"
+    },
+    {
+        "recipeName":"Green Soup",
+        "category": ["Dinner and Lunch", "NoMilk"],
+        "ingredients": ["4 potatos", "8 garlics", " 1 carrot", "4 collard greens", "1/2 chicken breasts", "salt"],
+        "description": 
+                        {1: "Chop the potatoes and carrot into cubes and cook in the pressure cooker for 15 minutes. Then put it to drain, set aside.",
+                        2: "Chop the chicken breast into large cubes, add the garlics and cook in the pressure cooker with 2 cups of water or enough water to cover the chicken.",        
+                        3: "Purée chicken, potatoes, carrot and collard greens with a blender",
+                        4: "Return it to a pan and boil for two minutes",
+                        5: "You can add other spices as you wish.",
+                    },
+        "imageUrl": "https://github.com/amandamigliorini/cse121b/blob/main/images/pexels-alesia-kozik-6072096.jpg"
+    },
 ]
